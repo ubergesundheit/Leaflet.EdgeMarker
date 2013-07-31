54b188d48dc5b676127005ec32bf4711652bfb4e
ubergesundheit <ubergesundheit@googlemail.com>
2013-07-31 12:34:47 +0200

    struggle with use strict

--- L.EdgeMarker.js
+++ L.EdgeMarker.js
@@ -1,4 +1,5 @@
 (function (L) {
+  "use strict";
 
   L.EdgeMarker = L.Class.extend({
 
@@ -6,11 +7,11 @@
       this._map = map;
 
       //add a method to get applicable features
-      L.extend(map,{
+      L.extend(map, {
         _getFeatures: function() {
-          out = [];
-          for(l in this._layers){
-                      console.log(this._layers[l]);
+          var out = [];
+          for(var l in this._layers){
+                      // console.log(this._layers[l]);
             //test if this is the markerpane, does this even work?
             if(typeof this._layers[l].getLatLng !== 'undefined'){
               out.push(this._layers[l]);
@@ -31,6 +32,8 @@
 
     onAdd: function (map) {},
 
+    _borderMarkerLayer: {},
+
     _addEdgeMarkers: function () {
        if(typeof _borderMarkerLayer === 'undefined') _borderMarkerLayer = new L.LayerGroup();
        _borderMarkerLayer.clearLayers();
@@ -38,30 +41,29 @@
        var features = this._map._getFeatures();
        console.log(features.length);
 
-       for(var i = 0; i < this._map.getPanes().markerPane.children.length; i++){
-         var markerPos = this._map.getPanes().markerPane.children[i].getBoundingClientRect();
+       for(var i = 0; i < features.length; i++){
+         var markerPos = this._map.latLngToContainerPoint(features[i].getLatLng());
          var bounds = this._map.getContainer().getBoundingClientRect();
+         console.log(markerPos);
+         console.log(bounds.height+" "+bounds.width);
+         console.log(markerPos.y < 0 || markerPos.y > bounds.height || markerPos.x > bounds.width || markerPos.x < 0);
+         if(markerPos.y < 0 || markerPos.y > bounds.height || markerPos.x > bounds.width || markerPos.x < 0){
 
-         if(markerPos.top < bounds.top || markerPos.bottom > bounds.bottom || markerPos.right > bounds.right || markerPos.left < bounds.left){
-           var markerPosInBounds = this._map.latLngToContainerPoint(this._map._getFeatures()[i].getLatLng());
-           if( markerPos.top < bounds.top ){ //oben raus
-             var y = 0;
-           } else if (markerPos.bottom > bounds.bottom) { //unten raus
-              var y = bounds.height;
-             
-           } else {
-             var y = markerPosInBounds.y;
-           }
+          var y = markerPos.y;
+          if( markerPos.y < 0 ){ //oben raus
+            y = 0;
+          } else if (markerPos.y > bounds.height) { //unten raus
+            y = bounds.height;   
+          }
 
-           if (markerPos.right > bounds.right) { // rechts raus
-             var x = bounds.width;
-           } else if ( markerPos.left < bounds.left) { // links raus
-             var x = 0;
-           } else {
-             var x = markerPosInBounds.x;
-           }
+          var x = markerPos.x;
+          if (markerPos.x > bounds.width) { // rechts raus
+            x = bounds.width;
+          } else if ( markerPos.x < 0) { // links raus
+            x = 0;
+          }
 
-           L.circleMarker(this._map.containerPointToLatLng([x,y]),{radius: 12,weight: 0,fillColor: 'blue',fillOpacity: 1}).addTo(_borderMarkerLayer);
+          L.circleMarker(this._map.containerPointToLatLng([x,y]),{radius: 12,weight: 0,fillColor: 'blue',fillOpacity: 1}).addTo(_borderMarkerLayer);
          }
        }
 
