5c139eaabdfcd9108d0a9748a8b17ffc63082738
ubergesundheit <ubergesundheit@googlemail.com>
2013-07-31 21:29:18 +0200

    refactoring, use of leaflet methods

--- Leaflet.EdgeMarker.js
+++ Leaflet.EdgeMarker.js
@@ -13,8 +13,6 @@
         _getFeatures: function () {
           var out = [];
           for (var l in this._layers) {
-
-            //test if this is the markerpane, does this even work?
             if(typeof this._layers[l].getLatLng !== 'undefined') {
               out.push(this._layers[l]);
             }
@@ -24,6 +22,7 @@
       });
 
       map.on('move', this._addEdgeMarkers, this);
+      map.on('viewreset', this._addEdgeMarkers, this);
 
       this._addEdgeMarkers();
 
@@ -43,31 +42,35 @@
       var features = this._map._getFeatures();
 
       for(var i = 0; i < features.length; i++) {
-        var markerPos = this._map.latLngToContainerPoint(features[i].getLatLng());
-        var bounds = this._map.getContainer().getBoundingClientRect();
+        var currentMarkerPosition = this._map.latLngToContainerPoint(
+                                                  features[i].getLatLng());
+        var mapPixelBounds = this._map.getSize();
 
-        if(markerPos.y < 0 || markerPos.y > bounds.height || 
-          markerPos.x > bounds.width || markerPos.x < 0){
+        if(currentMarkerPosition.y < 0 || 
+            currentMarkerPosition.y > mapPixelBounds.y || 
+            currentMarkerPosition.x > mapPixelBounds.x || 
+            currentMarkerPosition.x < 0) {
 
-          var y = markerPos.y;
-          if( markerPos.y < 0 ){ //oben raus
+          var y = currentMarkerPosition.y;
+          if( currentMarkerPosition.y < 0 ) { //oben raus
             y = 0;
-          } else if (markerPos.y > bounds.height) { //unten raus
-            y = bounds.height;   
+          } else if (currentMarkerPosition.y > mapPixelBounds.y) { //unten raus
+            y = mapPixelBounds.y;   
           }
 
-          var x = markerPos.x;
-          if (markerPos.x > bounds.width) { // rechts raus
-            x = bounds.width;
-          } else if ( markerPos.x < 0) { // links raus
+          var x = currentMarkerPosition.x;
+          if (currentMarkerPosition.x > mapPixelBounds.x) { // rechts raus
+            x = mapPixelBounds.x;
+          } else if ( currentMarkerPosition.x < 0) { // links raus
             x = 0;
           }
-
+          
           L.circleMarker(this._map.containerPointToLatLng([x,y]), {radius: 12,weight: 0,fillColor: 'blue',fillOpacity: 1}).addTo(this._borderMarkerLayer);
-         }
-       }
-
-       this._borderMarkerLayer.addTo(this._map);
+        }
+      }
+      if(!this._map.hasLayer(this._borderMarkerLayer)) {
+        this._borderMarkerLayer.addTo(this._map);
+      }
     }
   });
 
