8a428ded13a31afdd1d009e3901cb055db2a1e3b
ubergesundheit <ubergesundheit@googlemail.com>
2016-01-28 19:30:27 +0100

    update L.RotatedMarker of @bbecquet. Fixes wrong rotation in Leaflet 1.0

--- Leaflet.EdgeMarker.js
+++ Leaflet.EdgeMarker.js
@@ -92,11 +92,11 @@
           var y = currentMarkerPosition.y;
           var markerDistance;
 
-          // bottom out
+          // top out
           if (currentMarkerPosition.y < 0) {
             y = 0 + markerHeight / 2;
             markerDistance = -currentMarkerPosition.y;
-          // top out
+          // bottom out
           } else if (currentMarkerPosition.y > mapPixelBounds.y) {
             y = mapPixelBounds.y - markerHeight / 2;
             markerDistance = currentMarkerPosition.y - mapPixelBounds.y;
@@ -150,13 +150,24 @@
           angle: 0
       },
 
+      statics: {
+          TRANSFORM_ORIGIN: L.DomUtil.testProp(
+              ['transformOrigin', 'WebkitTransformOrigin', 'OTransformOrigin', 'MozTransformOrigin', 'msTransformOrigin'])
+      },
+
+      _initIcon: function() {
+          L.Marker.prototype._initIcon.call(this);
+
+          this._icon.style[L.RotatedMarker.TRANSFORM_ORIGIN] = '50% 50%';
+      },
+
       _setPos: function (pos) {
           L.Marker.prototype._setPos.call(this, pos);
 
           if (L.DomUtil.TRANSFORM) {
               // use the CSS transform rule if available
               this._icon.style[L.DomUtil.TRANSFORM] += ' rotate(' + this.options.angle + 'deg)';
-          } else if (L.Browser.ie) {
+          } else if(L.Browser.ie) {
               // fallback for IE6, IE7, IE8
               var rad = this.options.angle * (Math.PI / 180),
                   costheta = Math.cos(rad),
@@ -164,11 +175,15 @@
               this._icon.style.filter += ' progid:DXImageTransform.Microsoft.Matrix(sizingMethod=\'auto expand\', M11=' +
                   costheta + ', M12=' + (-sintheta) + ', M21=' + sintheta + ', M22=' + costheta + ')';
           }
+      },
+
+      setAngle: function (ang) {
+          this.options.angle = ang;
       }
-});
+  });
 
   L.rotatedMarker = function (pos, options) {
-    return new L.RotatedMarker(pos, options);
+      return new L.RotatedMarker(pos, options);
   };
 
   L.edgeMarker = function (options) {
