eb0b2abef78641c94f8fa020a29f8e5bca2f1bfe
Rens Sikma <de_kwibus@live.nl>
2017-03-15 12:18:23 +0100

    lot of changes should probly be split
    use roationMarker instead own
    correct EdgeMarker location
    remove nowlonger used code

--- Leaflet.EdgeMarker.js
+++ Leaflet.EdgeMarker.js
@@ -4,10 +4,6 @@
   L.EdgeMarker = L.Class.extend({
 
     options: {
-      distanceOpacity: false,
-      distanceOpacityFactor: 4,
-      layerGroup: null,
-      rotateIcons: true,
       icon: L.icon({
           iconUrl:  'images/edge-arrow-marker.png',
           clickable: true,
@@ -23,19 +19,6 @@
 
     addTo: function (map) {
       this._map = map;
-
-      // add a method to get applicable features
-      L.extend(map, {
-        _getFeatures: function () {
-          var out = [];
-          for (var l in this._layers) {
-            if (typeof this._layers[l].getLatLng !== 'undefined') {
-              out.push(this._layers[l]);
-            }
-          }
-          return out;
-        }
-      });
 
       map.on('move', this._addEdgeMarkers, this);
       map.on('viewreset', this._addEdgeMarkers, this);
@@ -58,100 +41,68 @@
       }
       this._borderMarkerLayer.clearLayers();
 
-      var mapPixelBounds = this._map.getSize();
-
-      var markerWidth = this.options.icon.options.iconSize[0];
-      var markerHeight = this.options.icon.options.iconSize[1];
-
+      var mapPixelBounds = L.bounds([0,0],this._map.getSize());
       var currentMarkerPosition = this._map.latLngToContainerPoint( this._target);
 
-        if (currentMarkerPosition.y < 0 ||
-            currentMarkerPosition.y > mapPixelBounds.y ||
-            currentMarkerPosition.x > mapPixelBounds.x ||
-            currentMarkerPosition.x < 0) {
+        if (currentMarkerPosition.y < mapPixelBounds.min.y ||
+            currentMarkerPosition.y > mapPixelBounds.max.y ||
+            currentMarkerPosition.x > mapPixelBounds.max.x ||
+            currentMarkerPosition.x < mapPixelBounds.min.x) {
 
             // get pos of marker
             var x = currentMarkerPosition.x;
             var y = currentMarkerPosition.y;
-            var markerDistance;
 
+            var markerWidth = this.options.icon.options.iconSize[0];
+            var markerHeight = this.options.icon.options.iconSize[1];
+
+            var center = mapPixelBounds.getCenter();
+
+
+            var rad = Math.atan2(center.y - y, center.x - x);
+            var rad2TopLeftcorner= Math.atan2(center.y-mapPixelBounds.min.y,center.x-mapPixelBounds.min.x)
+
+            // maker is in between diagonals window
+            if (Math.abs(rad) > rad2TopLeftcorner && Math.abs (rad) < Math.PI -rad2TopLeftcorner) {
+                x = center.x -  center.y/ Math.tan(Math.abs(rad));
+            }else {
+                if (x< center.x ){
+                    y = center.y -  center.x *Math.tan(rad);
+                }else{
+
+                    y = center.y +  center.x *Math.tan(rad);
+                }
+            }
+
+            // top out (top has y=0)
+            if (currentMarkerPosition.y < mapPixelBounds.min.y ) {
+                y = mapPixelBounds.min.y + markerHeight/2;
             // bottom out
-            if (currentMarkerPosition.y < 0) {
-                y = 0 + markerHeight / 2;
-                markerDistance = -currentMarkerPosition.y;
-                // top out
-            } else if (currentMarkerPosition.y > mapPixelBounds.y) {
-                y = mapPixelBounds.y - markerHeight / 2;
-                markerDistance = currentMarkerPosition.y - mapPixelBounds.y;
             }
-
+            else if (currentMarkerPosition.y > mapPixelBounds.max.y) {
+                y = mapPixelBounds.max.y - markerHeight/2 ;
+            }
             // right out
-            if (currentMarkerPosition.x > mapPixelBounds.x) {
-                x = mapPixelBounds.x - markerWidth / 2;
-                markerDistance = currentMarkerPosition.x - mapPixelBounds.x;
-                // left out
-            } else if (currentMarkerPosition.x < 0) {
-                x = 0 + markerWidth / 2;
-                markerDistance = -currentMarkerPosition.x;
+            if (currentMarkerPosition.x > mapPixelBounds.max.x) {
+                x = mapPixelBounds.max.x - markerWidth / 2;
+            // left out
+            } else if (currentMarkerPosition.x < mapPixelBounds.min.x) {
+                x = mapPixelBounds.min.x + markerWidth / 2;
             }
 
-            // change opacity on distance
-            var newOptions = this.options;
-            if (this.options.distanceOpacity) {
-                newOptions.fillOpacity = (100 - (markerDistance / this.options.distanceOpacityFactor)) / 100;
-            }
 
-            // rotate markers
-            if (this.options.rotateIcons) {
-                var centerX = mapPixelBounds.x / 2;
-                var centerY = mapPixelBounds.y / 2;
-                var angle = Math.atan2(centerY - y, centerX - x) / Math.PI * 180;
-                newOptions.angle = angle;
-            }
-
-            var ref = {latlng: this._target};
-            newOptions = L.extend({}, newOptions, ref);
-
-            var marker = L.rotatedMarker(this._map.containerPointToLatLng([x, y]), newOptions)
+            var marker = L.marker(this._map.containerPointToLatLng([x, y]), this.options)
                 .addTo(this._borderMarkerLayer);
 
             marker.on('click', this.onClick, marker);
+
+            marker.setRotationAngle(rad / Math.PI * 180);
         }
       if (!this._map.hasLayer(this._borderMarkerLayer)) {
         this._borderMarkerLayer.addTo(this._map);
       }
     }
   });
-
-
-  /*
-   * L.rotatedMarker class is taken from https://github.com/bbecquet/Leaflet.PolylineDecorator.
-   */
-  L.RotatedMarker = L.Marker.extend({
-      options: {
-          angle: 0
-      },
-
-      _setPos: function (pos) {
-          L.Marker.prototype._setPos.call(this, pos);
-
-          if (L.DomUtil.TRANSFORM) {
-              // use the CSS transform rule if available
-              this._icon.style[L.DomUtil.TRANSFORM] += ' rotate(' + this.options.angle + 'deg)';
-          } else if (L.Browser.ie) {
-              // fallback for IE6, IE7, IE8
-              var rad = this.options.angle * (Math.PI / 180),
-                  costheta = Math.cos(rad),
-                  sintheta = Math.sin(rad);
-              this._icon.style.filter += ' progid:DXImageTransform.Microsoft.Matrix(sizingMethod=\'auto expand\', M11=' +
-                  costheta + ', M12=' + (-sintheta) + ', M21=' + sintheta + ', M22=' + costheta + ')';
-          }
-      }
-});
-
-  L.rotatedMarker = function (pos, options) {
-    return new L.RotatedMarker(pos, options);
-  };
 
   L.edgeMarker = function (latlng, options) {
     return new L.EdgeMarker(latlng, options);
